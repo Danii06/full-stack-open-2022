20e3e6c3f27b53f9b521133c35e1eeb5d3b05288
Danii06 <daanniiionline+github@gmail.com>
2022-07-31 17:24:03 +0300

    Solution to Exercise 1.1
    Modify App.js, split App component into App, Header, Content and Total components.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,48 @@
 
 
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return props.content.map((part) => {
+    return <p key={part.name}>{part.name} {part.amount}</p>
+  });
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.content.reduce((prev, curr) => prev + curr.amount, 0)}</p>
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const data = {
+    name: 'Half Stack application development',
+    content: [
+      {
+        name: "Fundamentals of React",
+        amount: 10
+      },
+      {
+        name: "Using props to pass data",
+        amount: 7
+      },
+      {
+        name: "State of a component",
+        amount: 14
+      }
+    ]
+  }
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    <>
+      <Header name={data.name} />
+      <Content content={data.content} />
+      <Total content={data.content} />
+    </>
   )
 }
 
