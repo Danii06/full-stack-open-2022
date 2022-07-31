5029049574eb093bbfb88ba66ad4565024f8ff9e
Danii06 <daanniiionline+github@gmail.com>
2022-07-31 17:36:35 +0300

    Refactor App.js, Solution To Exercise 1.3, 1.4, 1.5
    Refactor App.js to match specified variable names for exercise 1.3, 1.4 and 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,51 +1,47 @@
 
 
 const Header = (props) => {
-  return (
-    <>
-      <h1>{props.name}</h1>
-    </>
-  )
+  return <h1>{props.name}</h1>
 }
 
 const Part = (props) => {
-  return <p>{props.data.name} {props.data.amount}</p>
+  return <p>{props.part.name} {props.exercises}</p>
 }
 
 const Content = (props) => {
-  return props.content.map((part) => {
-    return <Part key={part.name} data={part} />
+  return props.parts.map((part) => {
+    return <Part key={part.name} part={part} />
   });
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.content.reduce((prev, curr) => prev + curr.amount, 0)}</p>
+  return <p>Number of exercises {props.parts.reduce((prev, curr) => prev + curr.exercises, 0)}</p>
 }
 
 const App = () => {
-  const data = {
+  const course = {
     name: 'Half Stack application development',
-    content: [
+    parts: [
       {
         name: "Fundamentals of React",
-        amount: 10
+        exercises: 10
       },
       {
-        name: "Using props to pass data",
-        amount: 7
+        name: "Using props to pass course",
+        exercises: 7
       },
       {
         name: "State of a component",
-        amount: 14
+        exercises: 14
       }
     ]
   }
 
   return (
     <>
-      <Header name={data.name} />
-      <Content content={data.content} />
-      <Total content={data.content} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
