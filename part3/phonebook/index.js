a62e0f67bc0e577c117ab235d804e5f2c35070d5
Danii06 <daanniiionline+github@gmail.com>
2022-08-14 14:01:10 +0300

    Solution to Exercise 3.22, Add ESlint and fix style issues

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,137 +1,122 @@
-const database = require('./services/database')
-const express = require('express')
-const morgan = require('morgan')
-require('express')
+const database = require("./services/database")
+const express = require("express")
+const morgan = require("morgan")
+require("express")
 const app = express()
 
 database.connect()
 
 app.use(express.json())
 
-app.use(express.static('build'))
+app.use(express.static("build"))
 
 app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        JSON.stringify(req.body)
-    ].join(' ')
+	return [
+		tokens.method(req, res),
+		tokens.url(req, res),
+		tokens.status(req, res),
+		tokens.res(req, res, "content-length"), "-",
+		tokens["response-time"](req, res), "ms",
+		JSON.stringify(req.body)
+	].join(" ")
 }))
 
-app.get('/api/persons', (req, res) => {
-    database.getAll().then((data) => {
-        if (data === undefined) {
-            return {}
-        }
-        res.json(data)
-    })
+app.get("/api/persons", (req, res) => {
+	database.getAll().then((data) => {
+		if (data === undefined) {
+			return {}
+		}
+		res.json(data)
+	})
 })
 
 const handleIDExistence = (id) => {
-    return database.findID(id).then((person) => {
-        return [!!person,person]
-    })
+	return database.findID(id).then((person) => {
+		return [!!person, person]
+	})
 }
 
-app.get('/api/persons/:id', (req, res, next) => {
-    handleIDExistence(req.params.id, next).then(([exists, person])=>{
-        if(exists){
-            res.json(person)
-        }else{
-            res.status(404).json({ error: 'No such ID in phonebook' })
-        }
-    }).catch(error=>next(error))
+app.get("/api/persons/:id", (req, res, next) => {
+	handleIDExistence(req.params.id, next).then(([exists, person]) => {
+		if (exists) {
+			res.json(person)
+		} else {
+			res.status(404).json({ error: "No such ID in phonebook" })
+		}
+	}).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res, next) => {
-    handleIDExistence(req.params.id, next).then(([exists, person])=>{
-        if(exists){
-            database.del(req.params.id)
-            res.status(204).end()
-        }else{
-            res.status(404).json({ error: 'No such ID in phonebook' })
-        }
-    }).catch(error=>next(error))
+app.delete("/api/persons/:id", (req, res, next) => {
+	handleIDExistence(req.params.id, next).then(([exists,]) => {
+		if (exists) {
+			database.del(req.params.id)
+			res.status(204).end()
+		} else {
+			res.status(404).json({ error: "No such ID in phonebook" })
+		}
+	}).catch(error => next(error))
 })
 
 
-app.post('/api/persons', (req, res, next) => {
-    const data = req.body
-    // if (!("name" in data && "number" in data)) {
-    //     res.status(400).json({
-    //         error: "Name or Number missing"
-    //     })
-    //     return
-    // }
+app.post("/api/persons", (req, res, next) => {
+	const data = req.body
+	database.findName(data.name).then((names) => {
+		if (names.length !== 0) {
+			res.status(403).json({
+				error: "A record with that name already exists"
+			})
+		}
+		const newEntry = { name: data.name, number: data.number }
+		database.add(newEntry).then((person) => {
+			res.status(201).json(person)
+		}).catch((e) => next(e))
 
-    database.findName(data.name).then((names) => {
-        if (names.length !== 0) {
-            res.status(403).json({
-                error: "A record with that name already exists"
-            })
-        }
-        const newEntry = { name: data.name, number: data.number }
-        database.add(newEntry).then((person) => {
-            res.status(201).json(person)
-        }).catch((e)=>next(e))
-        
-    })
+	})
 
 })
 
-app.put('/api/persons/:id', (req, res, next) => {
-    const data = req.body
-    // if (!("name" in data && "number" in data)) {
-    //     res.status(400).json({
-    //         error: "Name or Number missing"
-    //     })
-    //     return
-    // }
+app.put("/api/persons/:id", (req, res, next) => {
+	const data = req.body
+	handleIDExistence(req.params.id, next).then(([exists, person]) => {
+		if (exists) {
+			database.updateByID(req.params.id, data).then(() => { data.id = person._id; res.status(200).json(data) }).catch(error => next(error))
+		} else {
+			res.status(404).json({ error: "No such ID in phonebook" })
+		}
+	})
 
-    handleIDExistence(req.params.id, next).then(([exists, person])=>{
-        if(exists){
-            database.updateByID(req.params.id, data).then(()=>{data.id=person._id;res.status(200).json(data)}).catch(error=>next(error))
-        }else{
-            res.status(404).json({ error: 'No such ID in phonebook' })
-        }
-    })
-
-    
 })
 
-app.get('/info', (req, res) => {
-    database.getAll().then((data) => {
-        if (data === undefined) {
-            return {}
-        }
-        res.send(`Phonebook has info for ${data.length} people<br>
+app.get("/info", (req, res) => {
+	database.getAll().then((data) => {
+		if (data === undefined) {
+			return {}
+		}
+		res.send(`Phonebook has info for ${data.length} people<br>
             ${new Date().toString()}`)
-    })
-    
+	})
+
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error("Error:", error.message)
+	console.error("Error:", error.message)
 
-    if (error.name === 'CastError') {
-        response.status(400).send({ error: 'Malformatted ID' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    
-    next(error)
+	if (error.name === "CastError") {
+		response.status(400).send({ error: "Malformatted ID" })
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message })
+	}
+
+	next(error)
 }
 
 app.use(errorHandler)
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
-    console.log(`Phonebook app listening on port ${port}`)
+	console.log(`Phonebook app listening on port ${port}`)
 })
 
-process.on('exit', function() {
-    database.disconnect()
-});
+process.on("exit", function () {
+	database.disconnect()
+})
--- part3/phonebook/services/database.js
+++ part3/phonebook/services/database.js
@@ -1,69 +1,69 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 const url = `mongodb+srv://daniad:${process.env.MONGODB_PASS}@cluster0.fk6a8ig.mongodb.net/?retryWrites=true&w=majority`
 
 const person = mongoose.Schema({
-    name:{
-        type:String,
-        minLength: 3,
-        required: true
-    },
-    number:{
-        type:String,
-        minLength:8,
-        validate:{
-            validator: (v)=>/^\d{2,3}-\d{1,}$/.test(v),
-            message: "Invalid phone number"
-        },
-        required: true
-    },
+	name:{
+		type:String,
+		minLength: 3,
+		required: true
+	},
+	number:{
+		type:String,
+		minLength:8,
+		validate:{
+			validator: (v) => /^\d{2,3}-\d{1,}$/.test(v),
+			message: "Invalid phone number"
+		},
+		required: true
+	},
 })
 
-person.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+person.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
-const Person = mongoose.model('Person',person)
+const Person = mongoose.model("Person",person)
 
 
 
 const connect = () => {
-    mongoose.connect(url)  
+	mongoose.connect(url)
 }
 
-const getAll = () => {  
-    return Person.find({}).then((res) => res)
+const getAll = () => {
+	return Person.find({}).then((res) => res)
 }
 
-const add = (person) => {  
-    return new Person(person).save()
+const add = (person) => {
+	return new Person(person).save()
 }
 
 const findID = (id) => {
-    return Person.findById(id)
+	return Person.findById(id)
 }
 
 const findName = (name) => {
-    return Person.find({name:name})
+	return Person.find({ name:name })
 }
 
 const updateByID = (id, data) => {
-    return Person.findByIdAndUpdate(id,data,{runValidators: true})
+	return Person.findByIdAndUpdate(id,data,{ runValidators: true })
 }
 
 const del = (id) => {
-    return findID(id).then((res) => {
-        res.delete()
-    })
+	return findID(id).then((res) => {
+		res.delete()
+	})
 }
 
 const disconnect = () => {
-    mongoose.connection.close()
+	mongoose.connection.close()
 }
 
-module.exports = {getAll, add, del, findID, findName, updateByID, connect, disconnect}
+module.exports = { getAll, add, del, findID, findName, updateByID, connect, disconnect }
