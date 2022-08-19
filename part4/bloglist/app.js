93369f9781867aa1532e92ed098148e0ce1cd245
Danii06 <daanniiionline+github@gmail.com>
2022-08-19 15:20:36 +0300

    Solution to Exercises 4.15,4.15
    Create api endpoint to add and view users

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -10,12 +10,15 @@ app.use(express.json())
 const blogsRouter = require("./controllers/bloglist.js")
 app.use("/api/blogs",blogsRouter)
 
+const usersRouter = require("./controllers/userController")
+app.use("/api/users",usersRouter)
+
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
 	logger.error(err.stack)
 	if(err.name === "ValidationError")
 		res.status(400).json({ error: err.message })
-	if(err.name === "CastError")
+	if(err.name === "CastError" || err.name == "")
 		res.status(400).json({ error: err.message })
 })
 
